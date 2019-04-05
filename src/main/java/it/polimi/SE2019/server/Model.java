24ee56f359f179dd692786e4eb9e724f4cb67bb8
Rodolfo Mariotti <rodolfo.mariotti@mail.polimi.it>
2019-04-05 15:54:59 +0200

    Added Model class

---
+++ src/main/java/it/polimi/SE2019/server/Model.java
@@ -0,0 +1,11 @@
+package it.polimi.SE2019.server;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Model extends Observable implements Observer {
+    @Override
+    public void update(Observable observable, Object o) {
+
+    }
+}
