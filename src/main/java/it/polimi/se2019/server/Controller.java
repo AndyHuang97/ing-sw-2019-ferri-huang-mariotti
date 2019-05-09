1138a39e39d3c7588f32a7201934359d17919374
Rodolfo Mariotti <rodo.mariotti@mail.polimi.it>
2019-05-09 10:01:44 +0200

    Added Controller draft

--- src/main/java/it/polimi/se2019/server/Controller.java
+++ src/main/java/it/polimi/se2019/server/Controller.java
@@ -4,14 +4,24 @@ package it.polimi.se2019.server;
 import it.polimi.se2019.client.View;
 import it.polimi.se2019.server.games.GameManager;
 import it.polimi.se2019.util.Observer;
+import it.polimi.se2019.util.Request;
 
 public class Controller implements Observer<PlayerAction> {
+
     private GameManager gameManager;
     private View view;
 
     public Controller(GameManager activeGames, View view) {
         this.gameManager = activeGames;
         this.view = view;
+    }
+
+    public void onNotify(Request request) {
+        /**
+         * Create an Action object by parsing the messageType and params list from
+         * the array, then run the action
+         */
+
     }
 
     public void ApplyAction(PlayerAction action){
